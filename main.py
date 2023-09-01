84e9801698320d298bdb82ab462a45c055d8a1ba
Mattan Tseng <mattantseng@Mattans-Mac-Studio.local>
2023-09-01 17:07:43 -0500

    Starting to put text in from introduction file

---
+++ main.py
@@ -0,0 +1,10 @@
+from src import load_cifar10, Net,
+training_step,evaluate
+if __name__ == '__main__':
+    n_epochs = 10
+    model = Net()
+    trainloader,testloader,classes = load_cifar10()
+    for epoch in range(n_epochs):
+    training_step(model, trainloader, epoch)
+    evaluate(model, testloader)
+    print("-"*10,"Training finshed","-"*10)
