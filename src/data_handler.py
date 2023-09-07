86b8445a236463fd4fb6f0d596281b04d0e96e9c
Mattan Tseng <mattan.tseng@gmail.com>
2023-09-06 20:33:06 -0500

    added validation set. Changed to snake caps

--- src/data_handler.py
+++ src/data_handler.py
@@ -1,25 +1,42 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from sklearn.model_selection import train_test_split
 
-def load_cifar10():
+
+def load_cifar10(hyperparameters: dict):
+    validation_ratio = hyperparameters["validation_ratio"]
+    train_ratio =  hyperparameters["train_ratio"]
+
+    batch_size = hyperparameters["batch_size"]
+    
     # Define transformations for the images
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+    
+    all_cifar_data = torchvision.datasets.CIFAR10(root='./data', train=True,
+                    download=True, transform=transform)
+    
+    
     # Download and load training dataset
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+    trainset, val_set = train_test_split(all_cifar_data, train_size=train_ratio, stratify=all_cifar_data.targets)
+    
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=2)
+
+    
+
     # Download and load test dataset
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
                                             shuffle=False, num_workers=2)
 
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    class_names = torchvision.datasets.CIFAR10.classes
     
-    return trainloader,testloader,classes
+    return train_loader,val_loader, test_loader,class_names, 
